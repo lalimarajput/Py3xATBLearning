468f2c2200ba2e35c0e8f67fc08b9dddddd7f188
lalima <lalimarajput@gmail.com>
2024-06-05 11:36:11 +0530

    Lab007

--- .venv/Ex-05062024/Lab007.py
+++ .venv/Ex-05062024/Lab007.py
@@ -1,12 +1,12 @@
 #Dynamically typed
 #To find the data typeof a function
-age=65
+age=65  #age=var name
 print(type(age))
 age="sixty five"
 print(type(age))
-isMale=True
+isMale=True   #isMale=var name
 print(type(isMale))
-Name="Lalima"
+Name="Lalima"  #Name=var name
 print(type(Name))
-Pi=3.14
+Pi=3.14   #Pi=var name
 print((type(Pi)))
---
+++ .venv/Ex-05062024/Lab008.py
@@ -0,0 +1,3 @@
+First_Name="Lalima"  #First_Nmae is a variable
+Last_Name="Rajput"    #Last_Name is a variable
+print(First_Name, Last_Name)
