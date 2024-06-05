c8aa02e0fb46a041f1584050e7e8e4bd78ddc4f0
lalima <lalimarajput@gmail.com>
2024-06-05 11:31:01 +0530

    Lab007

---
+++ .venv/Ex-05062024/Lab007.py
@@ -0,0 +1,12 @@
+#Dynamically typed
+#To find the data typeof a function
+age=65
+print(type(age))
+age="sixty five"
+print(type(age))
+isMale=True
+print(type(isMale))
+Name="Lalima"
+print(type(Name))
+Pi=3.14
+print((type(Pi)))
