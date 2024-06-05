ed0c511e35cf3c17b14c218160129329a8d4a7a9
lalima <lalimarajput@gmail.com>
2024-06-05 11:57:38 +0530

    Lab0010

---
+++ .venv/Ex-05062024/Lab010.py
@@ -0,0 +1,7 @@
+#Take a input from username and print it.
+Name = input("Enter your name: ")
+print("Your name is",Name)
+First_Name = input("Enter your first name: ")
+print("Your First Name is",First_Name )
+Last_Name = input("Enter your Last name: ")
+print("Your Last Name is", Last_Name)
