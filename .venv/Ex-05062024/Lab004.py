7aaa1298fa6f1cd4dba0d32f77451855284e403f
lalima <lalimarajput@gmail.com>
2024-06-05 11:13:37 +0530

    Lab004

--- .venv/Ex-05062024/Lab004.py
+++ .venv/Ex-05062024/Lab004.py
@@ -1,4 +1,7 @@
-print("Hello World", 123, True, 3.14)
-print("Hello World", 123, True, 3.14, sep='_')
-print("Hello World", 123, True, 3.14, sep='#')
-print("Hello World", 123, True, 3.14, sep=' ')
+print("Hi", "My name is Lalima", "Rajput") #3 arguments
+print("Hi My name is Lalima Rajput") #only 1 argument
+print("Hi My name is Lalima Rajput", end="_")
+print("Hi My name is Lalima Rajput", end="\n")
+print("Hi My name is Lalima Rajput")
+
+
