423abeaad8535af81640acba96ef3b15f8d1fa1b
lalima <lalimarajput@gmail.com>
2024-06-07 20:42:17 +0530

    Lab015

---
+++ EX-07062024/Lab015.py
@@ -0,0 +1,6 @@
+i = 10
+i2 = 12
+result = i2 / i
+print(result)
+print(type(result))  # python is very smart, it knows that the result will be a float value, so we don't have to
+# indicate float data type
