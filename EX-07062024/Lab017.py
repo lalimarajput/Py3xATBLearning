f24016181b90ac98b187f22fd6cc76283044c3fc
lalima <lalimarajput@gmail.com>
2024-06-07 20:57:13 +0530

    Lab018

---
+++ EX-07062024/Lab017.py
@@ -0,0 +1,7 @@
+# Format of the string
+first_name = "Harry"
+last_name = "Potter"
+print(first_name + "" + last_name)
+print(first_name + last_name)
+print(first_name, last_name)
+print(f'Your full name is {first_name} {last_name}')  # f is used for formatting
---
+++ EX-07062024/Lab018.py
@@ -0,0 +1,11 @@
+# Format string
+num = 90   # num is a variable
+print(f"The number is {num*2}")
+print(f"The number is {num*3}")
+
+num = 5  # table of 5
+print(f"{num}*1 = {num*1}")
+print(f"{num}*2 = {num*2}")
+print(f"{num}*3 = {num*3}")
+print(f"{num}*10 = {num*10}")
+
