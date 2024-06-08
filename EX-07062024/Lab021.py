a64c624d85d7dc8ab2ea2a7df7d98b8e3dd3f3ea
lalima <lalimarajput@gmail.com>
2024-06-08 12:52:03 +0530

    Lab021

--- EX-07062024/Lab021.py
+++ EX-07062024/Lab021.py
@@ -1,5 +1,6 @@
 # List
 my_list = [1,2,3]
+
 my_list.append(4)  # append() is use to add a single element at the end of the list
 print(my_list)
 
@@ -16,3 +17,29 @@ print(my_list)  # remove() is use to remove the first occurence of a specified v
 item = my_list.pop(1)  # pop() is use to remove the lst item
 print(item)
 print(my_list)
+
+my_list.clear()  # To clear the list
+print(my_list)
+
+my_list = [1,2,3,2]    # returns index of first occurence of a specific number
+item = my_list.index(2)  # index of 1 is 0 ,index of 2 is 1, index of 3 is 2....
+print(item)  #o/p = 1
+
+my_list = [1,2,3,2]
+item = my_list.count(2)  # returns count of the specific number
+print(item)  #o/p = 2
+
+my_list = [3,1,2]
+my_list.sort()  # sort in ascending order by default
+print(my_list)   # o/p = [1,2,3]
+
+my_list = [1,2,3]
+my_list.reverse()  # returns reverse order of the list
+print(my_list)
+
+my_list = [1,2,3]    # len() is a built-in function same as input() function
+length = len(my_list)  # returns length of the list
+print(length)   # o/p = 3
+
+
+
---
+++ EX-07062024/Lab022.py
@@ -0,0 +1,10 @@
+name = "This is a big line"
+print(len(name))    # length = 18
+print(type(name))   # type = str
+print(id(name))    # returns id
+print(name[14])      # returns letter of index value 14 i.e. l
+print(len('a'))
+print(len('is'))
+
+# space is also count for index value
+# like 0,1,2,3 for This and 4 for space 5,6 for is, 7 for space, 8 for a, 9 for space ,10,11, 12 for big, 13 for space, 14, 15,16,17 for line
