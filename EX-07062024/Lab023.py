d9b0486428f41b88aa6f3816f19be95559ad04e5
lalima <lalimarajput@gmail.com>
2024-06-08 13:06:07 +0530

    Lab024

---
+++ EX-07062024/Lab023.py
@@ -0,0 +1,7 @@
+list = ["milk", "butter", "bread"]
+print(len(list))   # 3
+print(list[0])    # Milk
+print(list[-1])    # Bread- last item  = -1
+
+list = [1,2,3,4, True, 3.14, "Lalima"]
+print(type(list))   # type of list is 'List'
---
+++ EX-07062024/Lab024.py
@@ -0,0 +1,5 @@
+# escape sequence
+
+print("Hello \"World\" ")  # Hello "World"
+print("Hello \n World")  # World starts with a new line
+print("Hello \t World")  # Hello   World
