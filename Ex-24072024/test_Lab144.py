6e0ac987b108cc32d008736b85fc5b2bd35ce516
lalima <lalimarajput@gmail.com>
2024-07-24 19:00:58 +0530

    Add Request module to project

---
+++ Ex-24072024/test_Lab144.py
@@ -0,0 +1,18 @@
+import pytest  # pip install pytest
+import allure  # pip install allure-pytest
+import requests  #install requests
+
+
+@allure.title("Test GET Request - RestFUL BOOKER Project#1")
+@allure.description("TC#1 -> Verify that GET Request with ID works")
+@allure.tag("regression", "p0", "smoke")
+@allure.label("owner", "Pramod Dutta")
+@allure.testcase("TC#5")
+def test_get_single_request_by_id():
+    url = "https://restful-booker.herokuapp.com/booking/1"
+    responseData = requests.get(url)
+    print(responseData.text)
+    print(responseData.headers)
+    print(responseData.cookies)
+    print(responseData.json())
+    assert responseData.status_code == 200
